75e3c9f5d23e14e7327ca8494f53186f3eba960f
Valery Louis <louisvalery2022@gmail.com>
2025-03-13 18:50:12 -0400

    refactored code

---
+++ file_sync.py
@@ -0,0 +1,55 @@
+# files in here will be shared with others
+public_file_names = []
+private_file_names = []
+
+def send_file():
+    '''use programming assignment 2 for reference'''
+    #TODO
+    pass
+
+def file_listener_server():
+    '''use programming assignment 2 for reference'''
+    #TODO
+    pass   
+
+def upload_file():
+    '''use programming assignment 2 for reference'''
+    #TODO
+    pass
+
+def get_file_size():
+    '''use programming assignment 2 for reference'''
+    #TODO
+    pass
+
+def get_file_info():
+    '''use programming assignment 2 for reference'''
+    #TODO
+    pass
+
+def view_public_files():
+    '''this method will be used to get the id of all the public files available to download on the network'''
+    print("Public files availabe to download: ", public_file_names)
+    #TODO
+    pass
+
+
+def view_private_files():
+    '''this method will be used to get the id of all the private files available to download on the network'''
+    print("Private files availabe to download: ", private_file_names)
+
+    #TODO
+    pass
+
+def add_to_public_files_list():
+    #TODO
+    pass
+
+def add_to_private_files_list():
+    #TODO
+    pass
+
+def request_file(file_id):
+    '''Do a broadcast with the file name and wait for a response from the peer that has the file'''
+    #TODO
+    pass
